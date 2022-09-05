ef38a399f1ffd6685f2c9802cf27a1125251b0f8
Konstantin Malanchev <hombit@gmail.com>
2022-09-05 12:45:53 -0500

    Refactor mcts checking

--- src/multicolor/multicolor_evaluator.rs
+++ src/multicolor/multicolor_evaluator.rs
@@ -9,10 +9,11 @@ pub use crate::float_trait::Float;
 pub use crate::multicolor::PassbandTrait;
 
 use enum_dispatch::enum_dispatch;
+use itertools::Itertools;
 pub use lazy_static::lazy_static;
 pub use schemars::JsonSchema;
 pub use serde::{Deserialize, Serialize};
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeSet;
 use std::fmt::Debug;
 
 #[enum_dispatch]
@@ -43,6 +44,38 @@ where
     }
 }
 
+enum InternalMctsError {
+    MultiColorEvaluatorError(MultiColorEvaluatorError),
+    InternalWrongPassbandSet,
+}
+
+impl InternalMctsError {
+    fn into_multi_color_evaluator_error<P, T>(
+        self,
+        mcts: &MultiColorTimeSeries<P, T>,
+        ps: &PassbandSet<P>,
+    ) -> MultiColorEvaluatorError
+    where
+        P: PassbandTrait,
+        T: Float,
+    {
+        match self {
+            InternalMctsError::MultiColorEvaluatorError(e) => e,
+            InternalMctsError::InternalWrongPassbandSet => {
+                MultiColorEvaluatorError::wrong_passbands_error(
+                    mcts.keys(),
+                    match ps {
+                        PassbandSet::FixedSet(ps) => ps.iter(),
+                        PassbandSet::AllAvailable => {
+                            panic!("PassbandSet cannot be ::AllAvailable here")
+                        }
+                    },
+                )
+            }
+        }
+    }
+}
+
 #[enum_dispatch]
 pub trait MultiColorEvaluator<P, T>:
     FeatureNamesDescriptionsTrait
@@ -54,11 +87,20 @@ where
     P: PassbandTrait,
     T: Float,
 {
+    /// Version of [MultiColorEvaluator::eval_multicolor] without basic [MultiColorTimeSeries] checks
+    fn eval_multicolor_no_mcts_check(
+        &self,
+        mcts: &mut MultiColorTimeSeries<P, T>,
+    ) -> Result<Vec<T>, MultiColorEvaluatorError>;
+
     /// Vector of feature values or `EvaluatorError`
     fn eval_multicolor(
         &self,
         mcts: &mut MultiColorTimeSeries<P, T>,
-    ) -> Result<Vec<T>, MultiColorEvaluatorError>;
+    ) -> Result<Vec<T>, MultiColorEvaluatorError> {
+        self.check_mcts(mcts)?;
+        self.eval_multicolor_no_mcts_check(mcts)
+    }
 
     /// Returns vector of feature values and fill invalid components with given value
     fn eval_or_fill_multicolor(
@@ -66,65 +108,145 @@ where
         mcts: &mut MultiColorTimeSeries<P, T>,
         fill_value: T,
     ) -> Result<Vec<T>, MultiColorEvaluatorError> {
-        self.check_mcts_shape(mcts)?;
         Ok(match self.eval_multicolor(mcts) {
             Ok(v) => v,
             Err(_) => vec![fill_value; self.size_hint()],
         })
     }
 
-    fn check_mcts_shape(
+    /// Check [MultiColorTimeSeries] to have required passbands and individual [TimeSeries] are valid
+    fn check_mcts(
         &self,
-        mcts: &MultiColorTimeSeries<P, T>,
-    ) -> Result<BTreeMap<P, usize>, MultiColorEvaluatorError> {
-        self.check_mcts_passabands(mcts)?;
-        self.check_every_ts_length(mcts)
+        mcts: &mut MultiColorTimeSeries<P, T>,
+    ) -> Result<(), MultiColorEvaluatorError> {
+        mcts.iter_passband_set_mut(self.get_passband_set())
+            .map(|(p, maybe_ts)| {
+                maybe_ts
+                    .ok_or(InternalMctsError::InternalWrongPassbandSet)
+                    .and_then(|ts| {
+                        self.check_ts(ts).map_err(|error| {
+                            InternalMctsError::MultiColorEvaluatorError(
+                                MultiColorEvaluatorError::MonochromeEvaluatorError {
+                                    error,
+                                    passband: p.name().into(),
+                                },
+                            )
+                        })
+                    })
+                    .map(|_| ())
+            })
+            .try_collect()
+            .map_err(|err| err.into_multi_color_evaluator_error(mcts, self.get_passband_set()))
+    }
+}
+
+#[cfg(test)]
+#[allow(clippy::unreadable_literal)]
+#[allow(clippy::excessive_precision)]
+mod tests {
+    use super::*;
+    use crate::data::TimeSeries;
+    use crate::multicolor::MonochromePassband;
+
+    use std::collections::BTreeMap;
+
+    #[derive(Clone, Debug, Serialize)]
+    struct TestTimeMultiColorFeature {
+        passband_set: PassbandSet<MonochromePassband<'static, f64>>,
     }
 
-    fn check_mcts_passabands(
-        &self,
-        mcts: &MultiColorTimeSeries<P, T>,
-    ) -> Result<(), MultiColorEvaluatorError> {
-        match self.get_passband_set() {
-            PassbandSet::AllAvailable => Ok(()),
-            PassbandSet::FixedSet(self_passbands) => {
-                if mcts
-                    .keys()
-                    .all(|mcts_passband| self_passbands.contains(mcts_passband))
-                {
-                    Ok(())
-                } else {
-                    Err(MultiColorEvaluatorError::wrong_passbands_error(
-                        mcts.keys(),
-                        self_passbands.iter(),
-                    ))
-                }
-            }
+    lazy_info!(
+        TEST_TIME_FEATURE_INFO,
+        TestTimeMultiColorFeature,
+        size: 1,
+        min_ts_length: 1,
+        t_required: true,
+        m_required: false,
+        w_required: false,
+        sorting_required: true,
+        variability_required: false,
+    );
+
+    impl FeatureNamesDescriptionsTrait for TestTimeMultiColorFeature {
+        fn get_names(&self) -> Vec<&str> {
+            vec!["zero"]
+        }
+
+        fn get_descriptions(&self) -> Vec<&str> {
+            vec!["zero"]
         }
     }
 
-    /// Checks if each component of [MultiColorTimeSeries] has enough points to evaluate the feature
-    fn check_every_ts_length(
-        &self,
-        mcts: &MultiColorTimeSeries<P, T>,
-    ) -> Result<BTreeMap<P, usize>, MultiColorEvaluatorError> {
-        // Use try_reduce when stabilizes
-        // https://github.com/rust-lang/rust/issues/87053
-        mcts.iter()
-            .map(|(passband, ts)| {
-                let length = ts.lenu();
-                if length < self.min_ts_length() {
-                    Err(MultiColorEvaluatorError::MonochromeEvaluatorError {
-                        error: EvaluatorError::ShortTimeSeries {
-                            actual: length,
-                            minimum: self.min_ts_length(),
-                        },
-                        passband: passband.name().into(),
-                    })
-                } else {
-                    Ok((passband.clone(), length))
-                }
-            })
-            .collect()
+    impl MultiColorPassbandSetTrait<MonochromePassband<'static, f64>> for TestTimeMultiColorFeature {
+        fn get_passband_set(&self) -> &PassbandSet<MonochromePassband<'static, f64>> {
+            &self.passband_set
+        }
+    }
+
+    impl<T> MultiColorEvaluator<MonochromePassband<'static, f64>, T> for TestTimeMultiColorFeature
+    where
+        T: Float,
+    {
+        fn eval_multicolor_no_mcts_check(
+            &self,
+            _mcts: &mut MultiColorTimeSeries<MonochromePassband<'static, f64>, T>,
+        ) -> Result<Vec<T>, MultiColorEvaluatorError> {
+            Ok(vec![T::zero()])
+        }
+    }
+
+    #[test]
+    fn test_check_mcts_passbands() {
+        let t = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
+        let m = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
+        let passband_b_capital = MonochromePassband::new(4400e-8, "B");
+        let passband_v_capital = MonochromePassband::new(5500e-8, "V");
+        let passband_r_capital = MonochromePassband::new(6400e-8, "R");
+        let mut mcts = {
+            let mut passbands = BTreeMap::new();
+            passbands.insert(
+                passband_b_capital.clone(),
+                TimeSeries::new_without_weight(&t, &m),
+            );
+            passbands.insert(
+                passband_v_capital.clone(),
+                TimeSeries::new_without_weight(&t, &m),
+            );
+            MultiColorTimeSeries::new(passbands)
+        };
+
+        let feature = TestTimeMultiColorFeature {
+            passband_set: PassbandSet::AllAvailable,
+        };
+        assert!(feature.eval_multicolor(&mut mcts).is_ok());
+
+        let feature = TestTimeMultiColorFeature {
+            passband_set: PassbandSet::FixedSet(
+                [passband_b_capital.clone(), passband_v_capital.clone()].into(),
+            ),
+        };
+        assert!(feature.eval_multicolor(&mut mcts).is_ok());
+
+        let feature = TestTimeMultiColorFeature {
+            passband_set: PassbandSet::FixedSet([passband_b_capital.clone()].into()),
+        };
+        assert!(feature.eval_multicolor(&mut mcts).is_ok());
+
+        let feature = TestTimeMultiColorFeature {
+            passband_set: PassbandSet::FixedSet([passband_r_capital.clone()].into()),
+        };
+        assert!(feature.eval_multicolor(&mut mcts).is_err());
+
+        let feature = TestTimeMultiColorFeature {
+            passband_set: PassbandSet::FixedSet(
+                [
+                    passband_b_capital.clone(),
+                    passband_r_capital.clone(),
+                    passband_r_capital.clone(),
+                ]
+                .into(),
+            ),
+        };
+        assert!(feature.eval_multicolor(&mut mcts).is_err());
     }
 }
